6420308de746bb37201b2b9ca6ded1654fbf69c2
Henry Doan <henrydoan96@gmail.com>
2020-02-25 20:52:36 -0700

    end of lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,8 +11,38 @@ class PagesController < ApplicationController
     @page = Page.new 
   end
 
+  def create
+    @page = Page.new(page_params)
+    
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new 
+    end
+  end
+
   def edit
     @page = Page.find(params[:id])
   end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 
 end
